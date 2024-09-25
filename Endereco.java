44b9deacc122a857abf6182efaa650ad99379d76
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-09-25 15:32:13 -0300

    animal

---
+++ Endereco.java
@@ -0,0 +1,43 @@
+package Aula_2509;
+
+public class Endereco{
+	private String rua;
+	private int num;
+	private String bairro;
+	private String cep;
+	private String cidade;
+	
+	public String getRua() {
+		return rua;
+		}
+	public void setRua(String rua) {
+		this.rua = rua;
+		}
+	public int getNum() {
+		return num;
+		}
+	public void setNum(int num) {
+		this.num = num;
+		}
+	public String getBairro() {
+		return bairro;
+		}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+		}
+	public String getCep() {
+		return cep;
+		}
+	public void setCep(String cep) {
+		this.cep = cep;
+		}
+	public String getCidade() {
+		return cidade;
+		}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+		}
+
+	}
+
+
---
+++ Estado.java
@@ -0,0 +1,26 @@
+package Aula_2509;
+public class Estado {
+		private String nome;
+		private String sigla;
+		
+		public Estado (String nome, String sigla) {
+			this.nome=nome;
+			this.sigla=sigla;
+		}
+		
+		public String getNome() {
+		return nome;
+		}
+		public void SetNome(String nome) {
+		this.nome = nome;
+		}
+		public String getSigla() {
+		return sigla;
+		}
+		public void setSigla (String sigla) {
+		this.sigla = sigla;
+
+
+	}
+
+}
